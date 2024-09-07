d409a8662a3698929d7ee8b77d8ca6c05334f2b8
singh2007 <75904126+singh2007@users.noreply.github.com>
2024-09-07 15:47:35 +0530

    Category.js

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,1 +1,18 @@
+import React from 'react';
+import Widget from './Widget';
+import { Typography } from '@mui/material';
 
+const Category = ({ category }) => {
+  return (
+    <div style={{ marginBottom: '20px' }}>
+      <Typography variant="h5">{category.categoryName}</Typography>
+      <div style={{ display: 'flex', gap: '20px' }}>
+        {category.widgets.map(widget => (
+          <Widget key={widget.widgetId} widget={widget} categoryId={category.id} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Category;
