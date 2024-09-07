6e4bb6746da7a613819f9d466340789a0ca80375
singh2007 <75904126+singh2007@users.noreply.github.com>
2024-09-07 15:48:00 +0530

    Widget.js

--- src/components/Widget.js
+++ src/components/Widget.js
@@ -1,1 +1,27 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { removeWidget } from '../redux/widgetSlice';
+import { Card, CardContent, Typography, IconButton } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
 
+const Widget = ({ widget, categoryId }) => {
+  const dispatch = useDispatch();
+
+  const handleRemoveWidget = () => {
+    dispatch(removeWidget({ categoryId, widgetId: widget.widgetId }));
+  };
+
+  return (
+    <Card style={{ width: '200px', padding: '10px' }}>
+      <CardContent>
+        <Typography variant="h6">{widget.widgetName}</Typography>
+        <Typography variant="body2">{widget.widgetText}</Typography>
+        <IconButton onClick={handleRemoveWidget} aria-label="remove">
+          <DeleteIcon />
+        </IconButton>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default Widget;
