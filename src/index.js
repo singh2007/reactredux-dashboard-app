489e77760296ec7b88065f3b972921f6530e6f51
singh2007 <75904126+singh2007@users.noreply.github.com>
2024-09-07 16:09:51 +0530

    index.js

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css'; // Optional: You can create this file for global styles
+import App from './App';
+import { store } from './redux/store';
+import { Provider } from 'react-redux';
+import reportWebVitals from './reportWebVitals';
+
+// Create the root for rendering the React app
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render the app and wrap it with the Redux Provider for state management
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
+
+// Optional: If you want to measure performance in your app, you can use this function
+// Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
