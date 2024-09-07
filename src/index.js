b83c332f5b190d39498b963853ce89555a000097
singh2007 <75904126+singh2007@users.noreply.github.com>
2024-09-07 16:22:53 +0530

    index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css'; // Optional: You can create this file for global styles
 import App from './App';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
-import reportWebVitals from './reportWebVitals';
+//import reportWebVitals from './reportWebVitals';
 
 // Create the root for rendering the React app
 const root = ReactDOM.createRoot(document.getElementById('root'));
