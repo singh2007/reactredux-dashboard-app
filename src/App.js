90db78a4a178dd37d916887fcd7e7550cd31d117
singh2007 <75904126+singh2007@users.noreply.github.com>
2024-09-07 15:46:53 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+import Dashboard from './components/Dashboard';
 
+function App() {
+  return (
+    <Provider store={store}>
+      <Dashboard />
+    </Provider>
+  );
+}
+
+export default App;
